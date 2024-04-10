c42fb271887d302b35f5fce1004012d8ff76bffb
gmvcruz <gmv.cruz.dev@gmail.com>
2024-04-10 21:14:16 +0200

    Added sound capture

--- functions.js
+++ functions.js
@@ -3,14 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
     let mediaRecorder;
 
     initButton.addEventListener('click', async () => {
-        if (initButton.value === "▶️ Iniciar") {
+        if (initButton.value === "▶️ Start") {
             // Iniciar la grabación
             const media = await navigator.mediaDevices.getDisplayMedia({
-                video: { 
+                video: {
                     width: { ideal: 3840, max: 3840 }, // Ancho ideal y máximo de 3840px (4K)
                     height: { ideal: 2160, max: 2160 }, // Altura ideal y máxima de 2160px (4K)
                     frameRate: { ideal: 60 } // Velocidad de cuadros ideal de 60fps
-                }
+                },
+                audio: true // Incluir captura de audio
+
             });
             mediaRecorder = new MediaRecorder(media, {
                 mimeType: 'video/webm;codecs=vp8,opus' // Utiliza el codec VP9 para obtener la mejor calidad
@@ -20,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const [video] = media.getVideoTracks();
             video.addEventListener("ended", () => {
                 mediaRecorder.stop();
-                initButton.value = "▶️ Iniciar";
+                initButton.value = "▶️ Start";
             });
 
             mediaRecorder.addEventListener("dataavailable", (e) => {
@@ -31,13 +33,13 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
             // Cambiar el valor del botón a "⏸ Detener"
-            initButton.value = "⏸ Detener";
+            initButton.value = "⏸ Stop";
         } else {
             // Detener la grabación
             mediaRecorder.stop();
 
             // Cambiar el valor del botón a "▶️ Iniciar"
-            initButton.value = "▶️ Iniciar";
+            initButton.value = "▶️ Start";
         }
     });
 });
